f634af197c867fc9f69377c7195ca6e1d7eae9b5
John Tweeddale <j.tweeddale@outlook.com>
2021-04-16 15:03:16 +0100

    parsed api response in place model and built basic response into search result cards

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,9 @@
 class Place < ApplicationRecord
+  require 'open-uri'
+  def self.api_search(location)
+    url = "https://api.teleport.org/api/cities/?search=#{location}"
+    read_url = URI.open(url).read
+    results = JSON.parse(read_url)
+    results["_embedded"]["city:search-results"]
+  end
 end
