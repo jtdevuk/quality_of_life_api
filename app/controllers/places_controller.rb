c1f015b1d0d6ebce6dfe9bf6c305033d3884b3e9
John Tweeddale <j.tweeddale@outlook.com>
2021-04-17 16:59:39 +0100

    build hash of search results in place model and pass to the view, simplify search results in view

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,15 @@
 class PlacesController < ApplicationController
+  before_action: :place_params, only: [create]
+
+  def create
+    @place = Place.new(place_params)
+    @place.save
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :housing, :cost_of_living, :safety)
+  end
+
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,27 +1,36 @@
 class Place < ApplicationRecord
   require 'open-uri'
   def self.api_search(place)
+    places = []
     url = "https://api.teleport.org/api/cities/?search=#{place}&embed=city%3Asearch-results%2Fcity%3Aitem%2Fcity%3Aurban_area%2Fua%3Ascores
     "
     read_url = URI.open(url).read
     results = JSON.parse(read_url)    
     unless results["_embedded"]["city:search-results"].empty?      
       # return an array of place objects for the view to iterate through
-      results["_embedded"]["city:search-results"].each do |result|
+      results["_embedded"]["city:search-results"].first(3).each do |result|
         city_item = result["_embedded"]["city:item"]
         if city_item["_embedded"]
-          name = city_item["_embedded"]["city:urban_area"]["full_name"]
-          categories = city_item["_embedded"]["city:urban_area"]["_embedded"]["ua:scores"]["categories"]
+          urban_area = city_item["_embedded"]["city:urban_area"]
+          name = urban_area["full_name"]
+          categories = urban_area["_embedded"]["ua:scores"]["categories"]
           housing = categories[0]["score_out_of_10"]
           cost_of_living = categories[1]["score_out_of_10"]
           safety = categories[7]["score_out_of_10"]
-          description = city_item["_embedded"]["city:urban_area"]["_embedded"]["ua:scores"]["summary"]
+          description = urban_area["_embedded"]["ua:scores"]["summary"]
         else
-          # name if there isn't any ["city:item"]["_embedded"]
-          # is ["_embedded"]["city:item"]["full_name"]
           name = city_item["full_name"]          
         end
+        places << {name: name, description: description, housing: housing, cost_of_living: cost_of_living, safety: safety}      
       end
     end
+    places
   end
 end
+
+
+# Place.create(name: name,
+#                      description: description,
+#                      housing: housing,
+#                      cost_of_living: cost_of_living,
+#                      safety: safety)
