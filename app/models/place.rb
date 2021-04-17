04a5355fe2078f5edfd12c80cdf7c717122d0a30
John Tweeddale <j.tweeddale@outlook.com>
2021-04-17 14:30:34 +0100

    retrieve data from api response in place model and assign to variables

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,27 @@
 class Place < ApplicationRecord
   require 'open-uri'
-  def self.api_search(location)
-    url = "https://api.teleport.org/api/cities/?search=#{location}"
+  def self.api_search(place)
+    url = "https://api.teleport.org/api/cities/?search=#{place}&embed=city%3Asearch-results%2Fcity%3Aitem%2Fcity%3Aurban_area%2Fua%3Ascores
+    "
     read_url = URI.open(url).read
-    results = JSON.parse(read_url)
-    results["_embedded"]["city:search-results"]
+    results = JSON.parse(read_url)    
+    unless results["_embedded"]["city:search-results"].empty?      
+      # return an array of place objects for the view to iterate through
+      results["_embedded"]["city:search-results"].each do |result|
+        city_item = result["_embedded"]["city:item"]
+        if city_item["_embedded"]
+          name = city_item["_embedded"]["city:urban_area"]["full_name"]
+          categories = city_item["_embedded"]["city:urban_area"]["_embedded"]["ua:scores"]["categories"]
+          housing = categories[0]["score_out_of_10"]
+          cost_of_living = categories[1]["score_out_of_10"]
+          safety = categories[7]["score_out_of_10"]
+          description = city_item["_embedded"]["city:urban_area"]["_embedded"]["ua:scores"]["summary"]
+        else
+          # name if there isn't any ["city:item"]["_embedded"]
+          # is ["_embedded"]["city:item"]["full_name"]
+          name = city_item["full_name"]          
+        end
+      end
+    end
   end
 end
