885d50ca73e45681e1390d5f05dcc87aa93cad01
John Tweeddale <j.tweeddale@outlook.com>
2021-04-18 17:49:11 +0100

    created route for adding a place to a user's places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,16 +1,25 @@
 class PlacesController < ApplicationController
   before_action :place_params, only: :create
+  before_action :set_place, only: [:show, :add_to_shortlist]
 
   def create
     @place = Place.new(place_params)
     @place.save
   end
 
-  def show
-    @place = Place.find(params[:id])
+  def show    
+  end
+
+  def add_to_shortlist
+    current_user.places << @place
+    redirect_to shortlist_path
   end
 
   private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
 
   def place_params
     params.require(:place).permit(:name, :summary, :housing, :cost_of_living, :safety)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "/places/:id", to: "places#show", as: "place"
+  post "/places/:id", to: "places#add_to_shortlist"
   get "/shortlist", to: "pages#shortlist", as: "shortlist"
 end
