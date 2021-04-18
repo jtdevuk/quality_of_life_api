4d94779515ee61b8aeb20dd851160d3b7536dc77
John Tweeddale <j.tweeddale@outlook.com>
2021-04-18 15:04:18 +0100

    created place show page route, controller action, and view

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,13 @@
 class PlacesController < ApplicationController
-  before_action: :place_params, only: [create]
+  before_action :place_params, only: :create
 
   def create
     @place = Place.new(place_params)
     @place.save
+  end
+
+  def show
+    @place = Place.find(params[:id])
   end
 
   private
