65ddba7b2973e05796abed2ca28f379a4ce535ef
John Tweeddale <j.tweeddale@outlook.com>
2021-04-18 14:48:20 +0100

    build a place object from each relevant search result

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -10,20 +10,22 @@ module PagesHelper
       # return an array of place objects for the view to iterate through
       results["_embedded"]["city:search-results"].first(3).each do |result|
         city_item = result["_embedded"]["city:item"]
-        if city_item["_embedded"]
-          urban_area = city_item["_embedded"]["city:urban_area"]
-          name = urban_area["full_name"]
-          categories = urban_area["_embedded"]["ua:scores"]["categories"]
-          housing = categories[0]["score_out_of_10"]
-          cost_of_living = categories[1]["score_out_of_10"]
-          safety = categories[7]["score_out_of_10"]
-          description = urban_area["_embedded"]["ua:scores"]["summary"]
-          places << {name: name, description: description, housing: housing, cost_of_living: cost_of_living, safety: safety}      
-        else
-          # name = city_item["full_name"]          
+        # The API response only includes relevant quality of life info when city_item["_embedded"] is present, other results are ignored
+        places << Place.create(build_place_from(city_item)) if city_item["_embedded"]
         end
       end
     end
     places
+  end
+
+  def build_place_from(city_item)
+    urban_area = city_item["_embedded"]["city:urban_area"]
+    name = urban_area["full_name"]
+    categories = urban_area["_embedded"]["ua:scores"]["categories"]
+    housing = categories[0]["score_out_of_10"]
+    cost_of_living = categories[1]["score_out_of_10"]
+    safety = categories[7]["score_out_of_10"]
+    summary = urban_area["_embedded"]["ua:scores"]["summary"]
+    { name: name, summary: summary, housing: housing, cost_of_living: cost_of_living, safety: safety }
   end
 end
