73c8d9d95e1e3e709d0d4daa9779adc5025f59c8
John Tweeddale <j.tweeddale@outlook.com>
2021-04-18 17:01:27 +0100

    added shortlist route, action and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
+  
   def home
+  end
+
+  def shortlist
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "/places/:id", to: "places#show", as: 'place'
+  get "/places/:id", to: "places#show", as: "place"
+  get "/shortlist", to: "pages#shortlist", as: "shortlist"
 end
