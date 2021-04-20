d132e23db627e6d51f15cde43da2fba0ab875de0
John Tweeddale <j.tweeddale@outlook.com>
2021-04-20 11:02:34 +0100

    move build_place_from method to places helper

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,18 @@
 module PlacesHelper
+  def build_place_from(city_item)
+    urban_area = city_item["_embedded"]["city:urban_area"]
+    name = urban_area["full_name"]
+    categories = urban_area["_embedded"]["ua:scores"]["categories"]
+    housing = categories[0]["score_out_of_10"]
+    cost_of_living = categories[1]["score_out_of_10"]
+    safety = categories[7]["score_out_of_10"]
+    summary = urban_area["_embedded"]["ua:scores"]["summary"]
+    # if the place already exists in the DB, return this existing place
+    if Place.any? { |p| p.name == name }
+      harry = Place.where(name: name).first      
+    else
+      # if the place doesn't exist, create a new one
+      Place.create({ name: name, summary: summary, housing: housing, cost_of_living: cost_of_living, safety: safety })
+    end
+  end
 end
